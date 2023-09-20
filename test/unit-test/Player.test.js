3fb1ae49e9e66ee7a20f76e4573213731c703bce
cobocho <rlaalsrb1111@naver.com>
2023-09-21 01:08:19 +0900

    Test: 마법 공격 관련 테스트코드 추가

--- test/unit-test/Player.test.js
+++ test/unit-test/Player.test.js
@@ -17,8 +17,21 @@ describe('플레이어 테스트', () => {
     expect(player.status.mp).toBe(80);
   });
 
-  it('플레이어는 유닛을 물리공격 할 시 유닛의 hp가 10 소모하고 플레이어의 mp가 10 회복한다.', () => {
+  it('플레이어는 기본 스킬로 물리 공격과 마법공격을 가진다.', () => {
+    expect(player.skills.size).toBe(2);
+    expect(player.skills.get(BasicAttack.SKILL_NAME)).toBeDefined();
+    expect(player.skills.get(MagicAttack.SKILL_NAME)).toBeDefined();
+  });
+
+  it('플레이어는 유닛을 물리 공격 할 시 유닛의 hp가 10 소모하고 플레이어의 mp가 10 회복한다.', () => {
     player.decreaseMp(30);
+    player.useSkill(BasicAttack.SKILL_NAME, enemy);
+
+    expect(enemy.status.hp).toBe(90);
+    expect(player.status.mp).toBe(60);
+  });
+
+  it('플레이어는 유닛을 마법 공격 할 시 유닛의 hp가 30 소모하고 플레이어의 mp가 30 소모한다.', () => {
     player.useSkill(BasicAttack.SKILL_NAME, enemy);
 
     expect(enemy.status.hp).toBe(90);
