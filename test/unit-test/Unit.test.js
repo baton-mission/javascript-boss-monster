86a64ffa9ab0e7c48be5eac82988315831a24dcc
cobocho <rlaalsrb1111@naver.com>
2023-09-20 22:32:27 +0900

    Test: `Unit` 관련 테스트코드 수정

--- test/unit-test/Unit.test.js
+++ test/unit-test/Unit.test.js
@@ -1,3 +1,5 @@
+import Unit from '../../src/domain/core/Unit';
+
 describe('유닛 테스트', () => {
   it('유닛은 체력을 보유한다.', () => {
     const unit = new Unit(100);
@@ -7,12 +9,12 @@ describe('유닛 테스트', () => {
   it('유닛은 hp를 소모할 수 있다.', () => {
     const unit = new Unit(100);
     unit.takeDamage(20);
-    unit.expect(unit.hp).toBe(80);
+    expect(unit.hp).toBe(80);
   });
 
   it('유닛은 hp는 0 이하로 떨어지지 않는다.', () => {
     const unit = new Unit(100);
     unit.takeDamage(110);
-    unit.expect(unit.hp).toBe(0);
+    expect(unit.hp).toBe(0);
   });
 });
--- test/unit/unit.test.js
+++
@@ -1,5 +0,0 @@
-describe('테스트', () => {
-  it('테스트', () => {
-    expect(true).toBeTruthy();
-  });
-});
