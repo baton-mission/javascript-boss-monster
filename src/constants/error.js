5d2db8a2fdb54f8fb1b4f0a1c86fd9c14dfe3d55
cobocho <rlaalsrb1111@naver.com>
2023-09-21 00:48:12 +0900

    Test: `Unit` 관련 테스트케이스 추가

---
+++ src/constants/error.js
@@ -0,0 +1,5 @@
+const ERROR_MESSAGE = Object.freeze({
+  EXISTING_SKILL: '이미 배운 스킬입니다!',
+});
+
+export default ERROR_MESSAGE;
--- test/unit-test/Unit.test.js
+++ test/unit-test/Unit.test.js
@@ -1,20 +1,63 @@
+import ERROR_MESSAGE from '../../src/constants/error';
+import Skill from '../../src/domain/core/Skill';
 import Unit from '../../src/domain/core/Unit';
 
 describe('유닛 테스트', () => {
+  let unit;
+
+  beforeEach(() => {
+    unit = new Unit({ name: '유닛', hp: 100, mp: 100 });
+  });
+
   it('유닛은 체력을 보유한다.', () => {
-    const unit = new Unit(100);
-    expect(unit.hp).toBe(100);
+    expect(unit.status.hp).toBe(100);
   });
 
-  it('유닛은 hp를 소모할 수 있다.', () => {
-    const unit = new Unit(100);
-    unit.takeDamage(20);
-    expect(unit.hp).toBe(80);
+  it('유닛은 스킬을 배울 수 있다.', () => {
+    expect(unit.skills.size).toBe(0);
+    unit.learnSkill('스킬', new Skill(unit, { skillName: '스킬', requireMp: 0 }));
+    expect(unit.skills.size).toBe(1);
   });
 
-  it('유닛은 hp는 0 이하로 떨어지지 않는다.', () => {
-    const unit = new Unit(100);
-    unit.takeDamage(110);
-    expect(unit.hp).toBe(0);
+  it('동일한 스킬을 배울 시 에러를 발생시킨다.', () => {
+    const skill = new Skill(unit, { skillName: '스킬', requireMp: 0 });
+    unit.learnSkill(skill.skillName, skill);
+
+    expect(() => {
+      unit.learnSkill(skill.skillName, skill);
+    }).toThrow(ERROR_MESSAGE.EXISTING_SKILL);
+  });
+
+  it('유닛은 스킬을 사용할 수 있다.', () => {
+    const enemy = new Unit({ name: '몬스터', hp: 100 });
+    const skill = new Skill(unit, { skillName: '스킬', requireMp: 0 });
+    unit.learnSkill('스킬', skill);
+
+    const skillSpy = jest.spyOn(skill, 'use');
+
+    unit.useSkill('스킬', enemy);
+
+    expect(skillSpy).toHaveBeenCalled();
+  });
+
+  it('유닛은 hp와 mp를 소모할 수 있다.', () => {
+    unit.decreaseHp(20);
+    unit.decreaseMp(20);
+    expect(unit.status.hp).toBe(80);
+    expect(unit.status.mp).toBe(80);
+  });
+
+  it('유닛의 hp와 mp는 최대값 이상으로 회복되지 않는다.', () => {
+    unit.increaseHp(10);
+    unit.increaseMp(10);
+    expect(unit.status.hp).toBe(100);
+    expect(unit.status.mp).toBe(100);
+  });
+
+  it('유닛의 hp와 mp는 0 이하로 떨어지지 않는다.', () => {
+    unit.decreaseHp(110);
+    unit.decreaseMp(110);
+    expect(unit.status.hp).toBe(0);
+    expect(unit.status.mp).toBe(0);
   });
 });
