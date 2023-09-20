aca5ea86b3539ff036b8a6bed40a72eb22681e10
cobocho <rlaalsrb1111@naver.com>
2023-09-20 22:26:01 +0900

    Test: `Unit` 관련 테스트 작성

---
+++ test/unit-test/Unit.test.js
@@ -0,0 +1,18 @@
+describe('유닛 테스트', () => {
+  it('유닛은 체력을 보유한다.', () => {
+    const unit = new Unit(100);
+    expect(unit.hp).toBe(100);
+  });
+
+  it('유닛은 hp를 소모할 수 있다.', () => {
+    const unit = new Unit(100);
+    unit.takeDamage(20);
+    unit.expect(unit.hp).toBe(80);
+  });
+
+  it('유닛은 hp는 0 이하로 떨어지지 않는다.', () => {
+    const unit = new Unit(100);
+    unit.takeDamage(110);
+    unit.expect(unit.hp).toBe(0);
+  });
+});
