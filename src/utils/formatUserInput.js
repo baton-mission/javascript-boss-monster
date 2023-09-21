b924625976679afe83118219cda2cd4760e1aeef
cobocho <rlaalsrb1111@naver.com>
2023-09-21 19:38:45 +0900

    Feat: 입력값 가공 로직 수정

--- src/utils/formatUserInput.js
+++ src/utils/formatUserInput.js
@@ -1,13 +1,16 @@
 /**
- * 유저의 입력값을 가공하는 메서드입니다. 양측단 공백을 제거하고, 입력값이 숫자일 경우 숫자를 반환합니다.
+ * 유저의 입력값을 가공하는 메서드입니다. 양측단 공백을 제거하고, formatToNumber가 true이면서 유효한 숫자값일 경우 숫자를 반환합니다.
  * @param {string} input
+ * @param {string} formatToNumber
  * @returns {string | number}
  */
-export const formatUserInput = (input) => {
+export const formatUserInput = (input, formatToNumber = false) => {
   if (!input.length) return input;
   let result = input;
   result = result.trim();
-  result = Number.isNaN(+result) ? result : +result;
+  if (Number.isNaN(+result)) {
+    return result;
+  }
 
-  return result;
+  return formatToNumber ? +result : result;
 };
