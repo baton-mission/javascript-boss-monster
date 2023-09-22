8b6cbcf55de64ad28b4bb77ee6e4c9963e75aa91
cobocho <rlaalsrb1111@naver.com>
2023-09-22 21:49:34 +0900

    Refactor: 스킬 관련 테스트 코드 분리

--- test/unit-test/BossMonster.test.js
+++ test/unit-test/BossMonster.test.js
@@ -1,4 +1,3 @@
-import { Player } from '../../src/domain/units';
 import { RandomAttack } from '../../src/domain/skills';
 import { Random } from '../../src/utils/random';
 import { ERROR_MESSAGE } from '../../src/constants/error';
@@ -9,12 +8,9 @@ jest.spyOn(Random, 'calculateBossDMG').mockReturnValue(13);
 describe('보스몬스터 테스트', () => {
   /** @type {BossMonster} */
   let monster;
-  /** @type {Player} */
-  let player;
 
   beforeEach(() => {
     monster = new BossMonster({ name: '보스 몬스터', hp: 100 });
-    player = new Player({ name: '용사', hp: 120, mp: 80 });
   });
 
   it('보스 몬스터는 이름, hp를 보유한다.', () => {
@@ -53,12 +49,6 @@ describe('보스몬스터 테스트', () => {
   it('보스 몬스터는 기본 스킬로 랜덤 데미지 공격을 가진다.', () => {
     expect(monster.skills.size).toBe(1);
     expect(monster.skills.get(RandomAttack.SKILL_NAME)).toBeDefined();
-  });
-
-  it('보스 몬스터는 유닛을 랜덤한 값으로 공격한다.', () => {
-    monster.useSkill(RandomAttack.SKILL_NAME, player);
-
-    expect(player.status.hp).toBe(107);
   });
 });
 
--- test/unit-test/Player.test.js
+++ test/unit-test/Player.test.js
@@ -1,17 +1,13 @@
 import { Player } from '../../src/domain/units';
 import { MagicAttack, PhysicalAttack } from '../../src/domain/skills';
 import { ERROR_MESSAGE } from '../../src/constants/error';
-import { BossMonster } from '../../src/domain/units/monsters/BossMonster';
 
 describe('플레이어 테스트', () => {
   /** @type {Player} */
   let player;
-  /** @type {BossMonster} */
-  let enemy;
 
   beforeEach(() => {
     player = new Player({ name: '용사', hp: 120, mp: 80 });
-    enemy = new BossMonster({ name: '몬스터', hp: 100 });
   });
 
   it('플레이어는 이름, hp, mp를 보유한다.', () => {
@@ -24,21 +20,6 @@ describe('플레이어 테스트', () => {
     expect(player.skills.size).toBe(2);
     expect(player.skills.get(PhysicalAttack.SKILL_NAME)).toBeDefined();
     expect(player.skills.get(MagicAttack.SKILL_NAME)).toBeDefined();
-  });
-
-  it('플레이어는 유닛을 물리 공격 할 시 유닛의 hp가 10 소모하고 플레이어의 mp가 10 회복한다.', () => {
-    player.decreaseMp(30);
-    player.useSkill(PhysicalAttack.SKILL_NAME, enemy);
-
-    expect(enemy.status.hp).toBe(90);
-    expect(player.status.mp).toBe(60);
-  });
-
-  it('플레이어는 유닛을 마법 공격 할 시 유닛의 hp가 20 소모하고 플레이어의 mp가 30 소모한다.', () => {
-    player.useSkill(MagicAttack.SKILL_NAME, enemy);
-
-    expect(enemy.status.hp).toBe(80);
-    expect(player.status.mp).toBe(50);
   });
 });
 
